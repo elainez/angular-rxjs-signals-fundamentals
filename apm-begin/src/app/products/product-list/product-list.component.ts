4e1a09644bafab6e9f9ea51b684ad18560f15fc6
elaine zhou <elaineyzhou28@gmail.com>
2025-06-10 10:07:18 +1200

    adding observable and error handling

--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { NgIf, NgFor, NgClass } from '@angular/common';
+import { NgIf, NgFor, NgClass, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { catchError, Observable, EMPTY } from 'rxjs';
 
 @Component({
-    selector: 'pm-product-list',
-    templateUrl: './product-list.component.html',
-    standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  standalone: true,
+  imports: [AsyncPipe, NgIf, NgFor, NgClass, ProductDetailComponent]
 })
 export class ProductListComponent {
   // Just enough here for the template to compile
   pageTitle = 'Products';
   errorMessage = '';
-
+  private productService = inject(ProductService);
   // Products
-  products: Product[] = [];
+  readonly products$ = this.productService.products$
+  .pipe(
+    catchError(error =>  {
+      this.errorMessage = error;
+      return EMPTY;
+    })
+  );
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { catchError, EMPTY, Observable, shareReplay, tap, throwError } from 'rxjs';
+import { Product } from './product';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorService } from '../utilities/http-error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +10,18 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   // Just enough here for the code to compile
   private productsUrl = 'api/products';
+  private http = inject(HttpClient);
+  private errorService = inject(HttpErrorService);
 
+  readonly products$ = this.http.get<Product[]>(this.productsUrl)
+    .pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      shareReplay(1),
+      catchError(error => this.handleError(error))
+    );
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    const formattedMesage = this.errorService.formatError(error);
+    return throwError(() => formattedMesage);
+  }
 }
